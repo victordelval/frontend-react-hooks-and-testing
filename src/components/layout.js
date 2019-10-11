3d38decbf8a72908890c7cab1478a0cf6c20e4b9
victordelval <victor.val.andres@gmail.com>
2019-10-11 18:36:30 +0200

    Add data provider to app and use data hook in layout for loading

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
 
+import { useData } from '../utils/dataHook'
 import Header from './header'
 import Footer from './footer'
+import Loader from './loader'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,12 +24,13 @@ const useStyles = makeStyles(theme => ({
 
 function Layout({ children }) {
   const classes = useStyles();
+  const { loading, error } = useData()
   return (
     <div className={classes.root}>
       <CssBaseline />
       <Header />
       <Container component="main" className={classes.main} maxWidth="sm">
-        {children}
+        {loading ? <Loader /> : children}
       </Container>
       <Footer />
     </div>
