9d44bfbbdfc4e3b6d3097d1bb627258ab0b95a41
victordelval <victor.val.andres@gmail.com>
2019-10-12 12:43:51 +0200

    Add timetable to professor sheet

--- src/pages/professor.js
+++ src/pages/professor.js
@@ -4,10 +4,15 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from '@material-ui/core/Paper';
+import Chip from '@material-ui/core/Chip';
 
 import { useData } from '../utils/dataHook'
+import Timetable from '../components/timetable'
 
 const useStyles = makeStyles(theme => ({
+  floatRight: {
+    float: 'right',
+  },
   detail: {
     padding: theme.spacing(3, 2),
   },
@@ -28,27 +33,30 @@ function Professor() {
       <Typography variant="h5" align="center" color="textSecondary" component="p">
         Select the professor of the subject you want the tutorship
       </Typography>
-      <br />
+      {/* <br /> */}
       <br />
       <Container maxWidth="md" component="main">
         <Selector data={professors} selected={selected} handleChange={handleChange} />
         {/* <hr /> */}
         <br />
-        <br />
+        {/* <br /> */}
         {selected && (
           <Paper className={classes.detail}>
             <Typography variant="h5" component="h3">
               {selected.name}
+              <span className={classes.floatRight}>
+                <Chip label={selected.area} />
+                {' '}
+                <Chip label={selected.office} />
+              </span>
             </Typography>
-            <Typography component="p">
-              {selected.area}
-            </Typography>
-            <Typography component="p">
-              {selected.office}
-            </Typography>
-            <Typography component="p">
-              {JSON.stringify(selected.schedules)}
-            </Typography>
+            <br />
+            {/* <Typography component="p">
+            </Typography> */}
+            {/* <Typography component="p"> */}
+              {/* {JSON.stringify(selected.schedules)} */}
+              <Timetable schedules={selected.schedules} />
+            {/* </Typography> */}
           </Paper>
         )}
       </Container>
